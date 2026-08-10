a07d15cec570cd67e39f42348bc251dc0a7dc6b0
Juan Jose Lozano <juan.lozano3@hotmail.com>
2026-08-10 11:31:24 -0500

    Updates

--- leetcode/arrays/0001_two_sum.py
+++ leetcode/arrays/0001_two_sum.py
@@ -1,11 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i = 0
-        while i < len(nums):
-            j = 0
-            while j < len(nums):
-                if (nums[i]+nums[j])==target and i!=j:
-                    return [i,j]
-                j+=1
-            i+=1
-        return "No"
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if((nums[i]+nums[j])==target and i!=j):
+                    return [i, j]
--- leetcode/arrays/3689_mamimum_total_subarray_value_i.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maxTotalValue(self, nums: List[int], k: int) -> int:
-        ma = max(nums)
-        mi = min(nums)
-        p = ma - mi
-        return p*k
-        
